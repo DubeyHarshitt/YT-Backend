93ae222dc20516cb94b96a5c747d41fd11bc5ad7
Harshit Dubey <122506643+DubeyHarshitt@users.noreply.github.com>
2024-04-30 20:57:57 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,6 +13,19 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}));  // json format me data lene k liye
 app.use(express.urlencoded({extended: true, limit: "16kb"})); //  Url se data lene k liye
 app.use(express.static("public")); // stored pdf's img's use krne k liye (ASSETS)
-app.use(express.cookieParser()); // cookie parser use krne k liye
+// app.use(express.cookieParser()); // cookie parser use krne k liye
+app.use(cookieParser());
+
+
+
+// Routes Import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
+
 
 export  {app} 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // There are two methods for this to be done 
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next)=>{
+    return (req, res, next)=>{
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
